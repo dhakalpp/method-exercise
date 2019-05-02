ea9622a88f398f885e87d582994c3b58ef7543cd
Priyanka Dhakal <30001935@student.toiohomai.ac.nz>
2019-05-02 13:50:16 +1200

    method exercise

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            /*Q1*/
+            Console.WriteLine("this appiliacation will change the sign of the number");
+            Console.WriteLine("please enter your number");
+            Console.WriteLine($" the number with its sign changed would be :{ChangeSign(double.Parse(Console.ReadLine()))}");
+
+            Console.ReadLine();
+
+            /*Q2*/
+            Console.WriteLine(" This application will convert miles to km's......");
+            Console.WriteLine("please enter a distance in miles");
+            double miles = double.Parse(Console.ReadLine());
+            Console.WriteLine($"{miles} = {Converter(miles)}KM's");
+        }
+         
+         
+
+
+    }
+}
